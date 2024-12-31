7ff83dd7c80600b57f85a5621f04d3fd98e608b2
shyirou <roidcoki@gmail.com>
2024-12-31 21:20:32 +0700

    add index.php

---
+++ web-app/app/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pilih Halaman Login</title>
+</head>
+<body>
+    <ul>
+        <li><a href="dashboard/dashboard.php">Login Pengguna</a></li>
+        <li><a href="login_adgov/check_session.php">Login Admin Pemerintah</a></li>
+    </ul>
+</body>
+</html> 
